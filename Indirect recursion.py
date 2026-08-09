e02e0e617019c342cbcf85e9fd4ee83dbfa141f5
Aishwarya Javvaji <aishwaryajavvaji869@gmail.com>
2026-08-09 19:21:33 +0530

    Add files via upload

---
+++ Indirect recursion.py
@@ -0,0 +1,15 @@
+#In-Direct Recursion
+#code to check the given numberis even or odd using In-direct recursion
+def even(n):
+    if n==0:
+        print("Even")
+        return
+    odd(n-1)
+def odd(n):
+    if n==0:
+        print("Odd")
+        return
+    even(n-1)
+n=int(input("Enter a number :"))
+even(n)
+    
