d5832c918c39aaabe20416387a64cadbb521e6f1
Aishwarya Javvaji <aishwaryajavvaji869@gmail.com>
2026-08-09 19:20:30 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,7 @@
+# arg pass return value
+def summate(a,b):
+    return a+b
+num1= int(input("Enter a first value"))
+num2= int(input("Enter a second value"))
+result= summate(num1,num2)
+print("Sum:",result)
