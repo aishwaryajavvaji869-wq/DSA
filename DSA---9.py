d7693acd97b130b4e53e192f0af92b9482faad08
Aishwarya Javvaji <aishwaryajavvaji869@gmail.com>
2026-08-09 19:12:41 +0530

    Add files via upload

---
+++ DSA---9.py
@@ -0,0 +1,36 @@
+num= int(input("Enter a number:"))
+print("Square of the number", num*num)
+
+
+n= int(input("Enter a number:"))
+for i in range(1,n+1):
+    print(i)
+    
+
+
+n= int(input("Enter a number:"))
+for i in range(1,n+1):
+    for j in range(1, n+1):
+        print(i, end=' ')
+    print()
+    
+
+n= int(input("Enter a number:"))
+while n>1:
+    print(n)
+    n//=2
+
+
+
+n= int(input("Enter a number:"))
+for i in range(n):
+    print(i, end=' ')
+for j in range(n):
+    print(j, end=' ')
+    
+    
+    
+
+n= int(input("Enter a number:"))
+for i in range(n//2):
+    print(i, end=' ')
