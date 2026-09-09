819f3e38ae944fb709426fc85377e04e2f7d1b64
Aishwarya Javvaji <aishwaryajavvaji869@gmail.com>
2026-09-09 16:55:11 +0530

    Add files via upload

---
+++ tree inorder.py
@@ -0,0 +1,28 @@
+#create a tree and traverse the tree inorder
+class node:
+    def __init__(self,data):
+        self.data= data
+        self.left= None
+        self.right= None
+values= input("Enter elements:")
+root= node(values[0])
+queue=[root]
+i=1
+while i<len(values):
+    current= queue.pop(0)
+    if i<len(values):
+        current.left= node(values[i])
+        queue.append(current.left)
+        i+=1
+    if i<len(values):
+        current.right= node(values[i])
+        queue.append(current.right)
+        i+=1
+print("Tree created...")
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.data, end='')
+        inorder(root.right)
+inorder(root)
+        
