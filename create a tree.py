607191b93f958ea6810e3b6cc293b52f5119ae93
Aishwarya Javvaji <aishwaryajavvaji869@gmail.com>
2026-09-09 17:11:30 +0530

    Add files via upload

---
+++ create a tree.py
@@ -0,0 +1,22 @@
+# create a tree
+class node:
+    def __init__(self,data):
+        self.data= data
+        self.left= None
+        self.right= None
+values= input("Enter elements:")
+root= node(values[0])
+queue=[root]
+i=1
+while i<len(values):
+    current= queue.pop(0)
+    if i<len(values):
+        current.left= node(values[i])
+        queue.append(current.left)
+        i+=1
+    if i<len(values):
+        current.right= node(values[i])
+        queue.append(current.right)
+        i+=1
+print("Tree created...")
+        
